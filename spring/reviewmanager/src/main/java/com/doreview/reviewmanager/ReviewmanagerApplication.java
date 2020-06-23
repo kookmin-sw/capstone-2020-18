4c194cbbe74091e908e92b023630c79df3020cf2
2Zero1 <Good7219@hotmail.com>
2020-06-24 00:24:51 +0900

    complete front

--- spring/reviewmanager/src/main/java/com/doreview/reviewmanager/ReviewmanagerApplication.java
+++ spring/reviewmanager/src/main/java/com/doreview/reviewmanager/ReviewmanagerApplication.java
@@ -7,7 +7,6 @@ import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ReviewmanagerApplication {
